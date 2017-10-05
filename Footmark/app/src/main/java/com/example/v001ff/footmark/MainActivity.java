9cca54cca7fca6fa06f0658e3f4a13916b45a6b2
v071ff <$>
2017-10-05 20:34:47 +0900

    aaaabbbb

--- Footmark/app/src/main/java/com/example/v001ff/footmark/MainActivity.java
+++ Footmark/app/src/main/java/com/example/v001ff/footmark/MainActivity.java
@@ -7,7 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-/*aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa*/
+/*aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+* bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb*/
 
 public class MainActivity extends AppCompatActivity {
 
