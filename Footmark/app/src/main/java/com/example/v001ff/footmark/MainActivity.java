0e1eb7c7c8e50d524e204c83ed6ce13de661518e
v001ff <v001ff@yamaguchi-u.ac.jp>
2017-10-11 15:14:53 +0900

    camera akagi

--- Footmark/app/src/main/java/com/example/v001ff/footmark/MainActivity.java
+++ Footmark/app/src/main/java/com/example/v001ff/footmark/MainActivity.java
@@ -1,31 +1,38 @@
 package com.example.v001ff.footmark;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /*aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa*/
 
 public class MainActivity extends AppCompatActivity {
+    final static private String TAG = "screen2camera";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new CameraFragment()).commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_setting){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
