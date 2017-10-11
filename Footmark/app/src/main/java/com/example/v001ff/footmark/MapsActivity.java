520ba74825f0d9777cba442fd29a030ff0110bbe
enPiT-T20 <v065ff@yamaguchi-u.ac.jp>
2017-10-11 15:30:23 +0900

    androidmanifestにpermissionを追加

--- Footmark/app/src/main/java/com/example/v001ff/footmark/MapsActivity.java
+++ Footmark/app/src/main/java/com/example/v001ff/footmark/MapsActivity.java
@@ -43,12 +43,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(yu).title("Marker in YU"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(yu));
     }
-/*  public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        LatLug yu = new LatLug(33.956416, 131.2725288);
-        mMap.addMaker(new MarkerOptions().position(yu).title("Marker in YU"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLug(yu));
-    }
- */
+
+    /*  public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
+            LatLug yu = new LatLug(33.956416, 131.2725288);
+            mMap.addMaker(new MarkerOptions().position(yu).title("Marker in YU"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLug(yu));
+        }
+     */
+    /*private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }*/
 
 }
