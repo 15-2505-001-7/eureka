5157ad7ba157bb3004f2336afb792bf19e5df5d3
v071ff <$>
2017-10-19 19:23:11 +0900

    実験場(MainActivity)にカメラ機能を試作 無事動きましたが、パーミッションの処理がうまくいかないため後日最終調整

--- Footmark/app/src/main/java/com/example/v001ff/footmark/MainActivity.java
+++ Footmark/app/src/main/java/com/example/v001ff/footmark/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.v001ff.footmark;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    //final static private String TAG = "screen2camera";
 
     static final int REQUEST_CAPTURE_IMAGE = 100;
     @Override
@@ -18,31 +22,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*if(savedInstanceState == null){
-           getSupportFragmentManager().beginTransaction().add(R.id.container, new CameraFragment()).commit();
-        }*/
+
         Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent();
-                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, REQUEST_CAPTURE_IMAGE);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {                        //カメラ起動するための処理。試作。
+
+                int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA);
+
+                if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+/*ここはエラー吐かれるので後々修正予定。パーミッションの処理です。
+                    // Android 6.0 のみ、カメラパーミッションが許可されていない場合
+                    final int REQUEST_CODE = 1;
+                    ActivityCompat.requestPermissions((Activity) this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE);
+
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                        // パーミッションが必要であることを明示するアプリケーション独自のUIを表示
+                    }
+*/
+                } else {
+                    // 許可済みの場合、もしくはAndroid 6.0以前
+                    // パーミッションが必要な処理。以下でカメラ起動。
+                    Intent intent = new Intent();
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAPTURE_IMAGE);
+                }
             }
         });
     }
 
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(REQUEST_CAPTURE_IMAGE == requestCode && resultCode == Activity.RESULT_OK){
+            Bitmap capturedImage = (Bitmap) data.getExtras().get("data");
+            ((ImageView) findViewById(R.id.image)).setImageBitmap(capturedImage);
+        }
+    }
+
     /*
     public void onClickButton(View view){
-        Intent intent = new Intent();
-        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, REQUEST_CAPTURE_IMAGE);
+        FloatingActionButton floatingActionButton = (FloatingActionButton)findViewById(R.id.button);
     }
     */
 
-    /*
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -53,7 +77,6 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
         if(id == R.id.action_setting){
             return true;
-    */
     @Override
     protected void onActivityResult(
             int requestCode,
@@ -64,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
             Bitmap capturedImage = (Bitmap) data.getExtras().get("data");
             ((ImageView) findViewById(R.id.image)).setImageBitmap(capturedImage);
         }
-        //return super.onOptionsItemSelected(item);
-    }
+        return super.onOptionsItemSelected(item);
+    }*/
 }
-
