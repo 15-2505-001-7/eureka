92df1462f850814be74dd5b87f67afa338cd7371
enPiT-P7 <v080ff@yamaguchi-u.ac.jp>
2017-10-23 17:21:49 +0900

    InputSpotActivityです

---
+++ Footmark/app/src/main/java/com/example/v001ff/footmark/InputSpotActivity.java
@@ -0,0 +1,13 @@
+package com.example.v001ff.footmark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputSpotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_spot);
+    }
+}
