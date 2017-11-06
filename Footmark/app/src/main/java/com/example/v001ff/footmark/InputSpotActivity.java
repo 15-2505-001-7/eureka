a4d68561da7647990de1a314c7d6408f62425020
v001ff <v001ff@yamaguchi-u.ac.jp>
2017-11-06 13:52:16 +0900

    Merge branch 'master' of https://github.com/15-2505-001-7/eureka
    # Conflicts:
    #	Footmark/app/src/main/res/layout/activity_maps.xml

--- Footmark/app/src/main/java/com/example/v001ff/footmark/InputSpotActivity.java
+++ Footmark/app/src/main/java/com/example/v001ff/footmark/InputSpotActivity.java
@@ -1,13 +1,62 @@
 package com.example.v001ff.footmark;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class InputSpotActivity extends AppCompatActivity {
+
+    private final int REQUEST_CAPTURE_IMAGE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_input_spot);
+
+        Button button = (Button) findViewById(R.id.spot_photo);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {                        //カメラ起動するための処理。試作。
+
+                int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA);
+
+                if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                    // Android 6.0 のみ、カメラパーミッションが許可されていない場合
+                    final int REQUEST_CODE = 1;
+                    ActivityCompat.requestPermissions(InputSpotActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE);            //修正予定ですごめんなさい
+
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(InputSpotActivity.this, Manifest.permission.CAMERA)) {                //修正予定ですごめんなさい
+                        // パーミッションが必要であることを明示するアプリケーション独自のUIを表示
+                        Snackbar.make(view, R.string.rationale, Snackbar.LENGTH_LONG).show();
+                    }
+                } else {
+                    // 許可済みの場合、もしくはAndroid 6.0以前
+                    // パーミッションが必要な処理。以下でカメラ起動。
+                    Intent intent = new Intent();
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAPTURE_IMAGE);
+
+                }
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(REQUEST_CAPTURE_IMAGE == requestCode && resultCode == Activity.RESULT_OK){
+            Bitmap capturedImage = (Bitmap) data.getExtras().get("data");
+            ((ImageView) findViewById(R.id.image)).setImageBitmap(capturedImage);
+        }
+    }
+
 }
--- Footmark/app/src/main/java/com/example/v001ff/footmark/InputSpotFragment.java
+++ Footmark/app/src/main/java/com/example/v001ff/footmark/InputSpotFragment.java
@@ -13,7 +13,6 @@ public class InputSpotFragment extends Fragment{
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    static final int REQUEST_CAPTURE_IMAGE = 100; //
 
     // TODO: Rename and change types of parameters
     private String mParam1;
