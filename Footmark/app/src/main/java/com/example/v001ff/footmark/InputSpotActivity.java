6e00fabeee1c6cff397d269438f13271e6de8354
v071ff <$>
2017-11-06 14:55:01 +0900

    InputSpotActivityちょっと変更

--- Footmark/app/src/main/java/com/example/v001ff/footmark/InputSpotActivity.java
+++ Footmark/app/src/main/java/com/example/v001ff/footmark/InputSpotActivity.java
@@ -10,8 +10,8 @@ import io.realm.Realm;
 public class InputSpotActivity extends AppCompatActivity {
     private Realm mRealm;                                       //このオブジェクトはDB更新に使う
 
-    private EditText editPlaceName;                             //投稿画面の場所の名前入力部分に対応
-    private EditText editReview;                                //投稿画面のレビュー部分に対応
+    private EditText mAddPlaceName;                             //投稿画面の場所の名前入力部分に対応
+    private EditText mAddReview;                                //投稿画面のレビュー部分に対応
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +19,11 @@ public class InputSpotActivity extends AppCompatActivity {
         setContentView(R.layout.fragment_input_spot);
 
         mRealm = Realm.getDefaultInstance();                    //Realmを使用する準備。Realmクラスのインスタンスを取得している
+        mAddPlaceName = (EditText) findViewById(R.id.addPlaceName);
+        mAddReview = (EditText) findViewById(R.id.addReview);
     }
 
-    public void oninputTapped(View view) {
-        editPlaceName = (EditText) findViewById(R.id.addPlaceName);
-        editReview = (EditText) findViewById(R.id.addReview);
+    public void onpostingButtonTapped(View view) {
 
         //ここにRealmにデータ追加する文を書く
         //あとボタンの名前をinputから変えたほうがいい
