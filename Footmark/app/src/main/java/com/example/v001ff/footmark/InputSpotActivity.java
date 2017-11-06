a0e634a0052247fe8a80270908c0bd4582c3b6b1
v001ff <v001ff@yamaguchi-u.ac.jp>
2017-11-06 15:02:09 +0900

    Merge branch 'master' of https://github.com/15-2505-001-7/eureka
    # Conflicts:
    #	Footmark/app/src/main/java/com/example/v001ff/footmark/InputSpotActivity.java
    #	Footmark/app/src/main/java/com/example/v001ff/footmark/InputSpotFragment.java

--- Footmark/app/src/main/java/com/example/v001ff/footmark/InputSpotActivity.java
+++ Footmark/app/src/main/java/com/example/v001ff/footmark/InputSpotActivity.java
@@ -1,5 +1,10 @@
 package com.example.v001ff.footmark;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
@@ -8,11 +13,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import io.realm.Realm;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 public class InputSpotActivity extends AppCompatActivity {
     private Realm mRealm;                                       //このオブジェクトはDB更新に使う
@@ -20,12 +23,40 @@ public class InputSpotActivity extends AppCompatActivity {
     private EditText editPlaceName;                             //投稿画面の場所の名前入力部分に対応
     private EditText editReview;                                //投稿画面のレビュー部分に対応
 
+    static final int REQUEST_CAPTURE_IMAGE = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_input_spot);
 
         mRealm = Realm.getDefaultInstance();                    //Realmを使用する準備。Realmクラスのインスタンスを取得している
+
+        ImageView spot_photo = (ImageView) findViewById(R.id.spot_photo);
+        spot_photo.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {                        //カメラ起動するための処理。試作。
+
+                int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA);
+
+                if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                    // Android 6.0 のみ、カメラパーミッションが許可されていない場合
+                    final int REQUEST_CODE = 1;
+                    ActivityCompat.requestPermissions(InputSpotActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE);            //修正予定ですごめんなさい
+
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(InputSpotActivity.this, Manifest.permission.CAMERA)) {                //修正予定ですごめんなさい
+                        // パーミッションが必要であることを明示するアプリケーション独自のUIを表示
+                        Snackbar.make(view, R.string.rationale, Snackbar.LENGTH_LONG).show();
+                    }
+                } else {
+                    // 許可済みの場合、もしくはAndroid 6.0以前
+                    // パーミッションが必要な処理。以下でカメラ起動。
+                    Intent intent = new Intent();
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAPTURE_IMAGE);
+
+                }
+            }
+        });
     }
 
     public void oninputTapped(View view) {
@@ -41,5 +72,13 @@ public class InputSpotActivity extends AppCompatActivity {
     public void onDestroy() {
         super.onDestroy();
         mRealm.close();                         //投稿画面から離れるときにDBのリソース開放
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(REQUEST_CAPTURE_IMAGE == requestCode && resultCode == Activity.RESULT_OK){
+            Bitmap capturedImage = (Bitmap) data.getExtras().get("data");
+            ((ImageView) findViewById(R.id.image)).setImageBitmap(capturedImage);
+        }
     }
 }
