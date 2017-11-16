44dd6fe26da03606f4b5ea0c8cae598575c0c55e
enPiT-T20 <v065ff@yamaguchi-u.ac.jp>
2017-11-16 14:41:15 +0900

    a

--- Footmark/app/src/main/java/com/example/v001ff/footmark/PostRealmAdapter.java
+++ Footmark/app/src/main/java/com/example/v001ff/footmark/PostRealmAdapter.java
@@ -1,19 +1,33 @@
 package com.example.v001ff.footmark;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import io.realm.OrderedRealmCollection;
+import io.realm.RealmRecyclerViewAdapter;
+
 /**
  * Created by enPiT-P7 on 2017/11/16.
  */
 
-public class PostRealmAdapter  /*extends RealmRecyclerViewAdapter<Post, PostRealmAdapter.PostViewHolder>*/ {
-   /* Context context;
+public class PostRealmAdapter  extends RealmRecyclerViewAdapter<Post, PostRealmAdapter.PostViewHolder> {
+    Context context;
 
-    public static class PostViewHolder extends RecyclerView.ViewHolder{
+    public static class PostViewHolder extends RecyclerView.ViewHolder {
         protected TextView userName;
         protected TextView spoyInfo;
         protected TextView date;
         protected ImageView userPhoto;
 
-        public PostViewHolder(View itemView){
+        public PostViewHolder(View itemView) {
             super(itemView);
             userName = (TextView) itemView.findViewById(R.id.username);
             spoyInfo = (TextView) itemView.findViewById(R.id.spotinfo);
@@ -29,9 +43,9 @@ public class PostRealmAdapter  /*extends RealmRecyclerViewAdapter<Post, PostReal
     }
 
     @Override
-    public SpotViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+    public PostViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_layout, parent, false);
-        final SpotViewHolder holder = new SpotViewHolder(itemView);
+        final PostViewHolder holder = new PostViewHolder(itemView);
 
         /*holder.itemView.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -45,23 +59,19 @@ public class PostRealmAdapter  /*extends RealmRecyclerViewAdapter<Post, PostReal
                 context.startActivity(intent);
             }
         });*/
-       /* return holder;
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(PostViewHolder holder, int position) {
+        Post post = getData().get(position);
+        holder.userName.setText(post.userName);
+        holder.spoyInfo.setText(post.spoyInfo);
+        holder.date.setText(post.date);
+        if (post.userPhoto != null && post.userPhoto.length != 0) {
+            Bitmap bmp = MyUtils.getImageFromByte(post.userPhoto);
+            holder.userPhoto.setImageBitmap(bmp);
 
-    }
-
-    @Override
-    public void onBindViewHolder(SpotViewHolder holder, int position){
-        Spot spot = getData().get(position);
-        holder.userName.setText(spot.username);
-        holder.spoyInfo.setText(spot.spotinfo);
-        holder.date.setText(spot.date);
-        if(spot.image != null && spot.image.length != 0){
-            Bitmap bmp = MyUtils.getImageFromByte(spot.image);
-            holder.spot.setImageBitmap(bmp);
         }
-    }*/
+    }
 }
